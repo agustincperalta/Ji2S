0ad87baa8e12e816823d36700bb3ffa88e1fc7cd
Agustín Peralta <agusmx77@gmail.com>
2019-06-09 08:06:40 -0500

    Agrega la pregunta 7 del capitulo 6: appropiate array

---
+++ src/Questions6/Question7.java
@@ -0,0 +1,21 @@
+package Questions6;
+/*
+7. Consider the following scenarios and, for each,
+declare the appropiate array:
+
+a) goals: an array to hold the number of goals each
+team in a league scores in each game of the season.
+The league consist of 20 teams and a season consists
+of 38 games
+b) seats: an array to record whether or not a seat in
+a theatre is booked or not. There are 70 rows of seats
+in the theatre and each row has 20 seats.
+
+        boolean seats[][] = new boolean*/
+public class Question7 {
+    public static void main(String[] args) {
+        int goals[][] = new int[20][38]; // a
+        boolean seats[][] = new boolean[70][20]; // b
+
+    }
+}
