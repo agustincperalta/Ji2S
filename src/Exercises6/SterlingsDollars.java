542b932787954917d6d214f24a0a0fe9fb4098f7
Agustín Peralta <agusmx77@gmail.com>
2019-06-12 22:42:05 -0500

    Agrega el ejercicio 7 del capitulo 6: SterlingsDollars + calculateTotal + how many items purchase

--- src/Exercises6/SterlingsDollars.java
+++ src/Exercises6/SterlingsDollars.java
@@ -7,31 +7,51 @@ pounds to dollars, and should, when it terminates, display the contents of both
 arrays. Once again, make use of methods in your program to carry out these
 tasks.*/
 
+/*
+ * Amend the program above so that
+ * (a) the user is asked how many items they wish to purchase and the arrays
+ * are then sized accordingly;
+ *
+ * (b) the total cost of the order is displayed in both currencies
+ * */
+
 import java.util.Scanner;
 
 public class SterlingsDollars {
     public static void main(String[] args) {
-        double[] productsPounds = new double[5];
-        double[] productsDollars = new double[5];
-        double exchangeRate;
         Scanner kbd = new Scanner(System.in);
+        int size;
+        do {
+            System.out.println("How many items do you want to purchase?");
+            size = kbd.nextInt();
+            if (size <= 0) {
+                System.out.println("Invalid value");
+                System.out.println("Try again");
+            }
+        } while (size <= 0);
+        double[] productsYen = new double[size];
+        double[] productsDollars = new double[size];
+        double exchangeRate;
 
-        for (int i = 0; i < productsPounds.length; i++) {
-            System.out.println("Enter the product (£)" + (i + 1) + ":");
-            productsPounds[i] = kbd.nextDouble();
+        for (int i = 0; i < productsYen.length; i++) {
+            System.out.println("Enter the product (¥)" + (i + 1) + ":");
+            productsYen[i] = kbd.nextDouble();
         }
         System.out.println("Please enter the exchange rate:");
         exchangeRate = kbd.nextDouble();
 
 
         // Copy the arrayPounds
-        for (int i = 0; i < productsPounds.length; i++) {
-            productsDollars[i] = productsPounds[i] * exchangeRate;
+        for (int i = 0; i < productsYen.length; i++) {
+            productsDollars[i] = productsYen[i] * exchangeRate;
         }
 
-        displayArray(productsPounds);
+        displayArray(productsYen);
         System.out.println();
         displayArray(productsDollars);
+        System.out.println();
+        System.out.println("The first total is: " + calculateTotal(productsYen));
+        System.out.println("The second total is: " + calculateTotal(productsDollars));
     }
 
     static void displayArray(double... arrayIn) {
@@ -41,5 +61,13 @@ public class SterlingsDollars {
 
 
         }
+    }
+
+    static double calculateTotal(double ... arrayIn){
+        double total = 0;
+        for (double i: arrayIn) {
+            total += i;
+        }
+        return total;
     }
 }
