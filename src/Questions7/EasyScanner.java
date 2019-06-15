1714565c85234c42074cd3f401655a31c935bb1c
Agustín Peralta <agusmx77@gmail.com>
2019-06-15 14:09:44 -0500

    Agrega Ejercicio 9 del capitulo 7: nextByte() & nextLong() added to EasyScanner

--- src/Questions7/EasyScanner.java
+++ src/Questions7/EasyScanner.java
@@ -1,34 +1,45 @@
 package Questions7;
+/*
+* 9. Add some additional methods such as nextByte and nextLong to the
+EasyScanner class
+* */
 
 import java.util.Scanner;
 
-public class EasyScanner
-{
-	public static int nextInt()
-	{
-		Scanner sc = new Scanner(System.in);
-		int i = sc.nextInt();
-		return i;
-	}
-	
-	public static double nextDouble()
-	{
-		Scanner sc = new Scanner(System.in);
-		double d = sc.nextDouble();
-		return d;
-	}
-	
-	public static String nextString()
-	{
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		return s;
-	}
-	
-	public static char nextChar()
-	{
-		Scanner sc = new Scanner(System.in);
-		char c = sc.next().charAt(0);
-		return c;
-	}
+public class EasyScanner {
+    public static byte nextByte() {
+        Scanner sc = new Scanner(System.in);
+        byte b = sc.nextByte();
+        return b;
+    }
+
+    public static long nextLong() {
+        Scanner sc = new Scanner(System.in);
+        long l = sc.nextLong();
+        return l;
+    }
+
+    public static int nextInt() {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        return i;
+    }
+
+    public static double nextDouble() {
+        Scanner sc = new Scanner(System.in);
+        double d = sc.nextDouble();
+        return d;
+    }
+
+    public static String nextString() {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        return s;
+    }
+
+    public static char nextChar() {
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+        return c;
+    }
 }
